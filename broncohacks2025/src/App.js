20b527740e9be18ebb98d3cbc37957db9eddb729
ClavinK <ckama222@gmail.com>
2025-04-18 18:50:33 -0700

    Basic Popup

--- broncohacks2025/src/App.js
+++ broncohacks2025/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from './files/Button';
+import Popup from './Popup';
+import React, { useState } from 'react';
 
 function App() {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
   return (
     <div>
-      <Button />
+      {/* <Button /> */}
+      <button onClick={() => setIsPopupOpen(true)}>Show Popup</button>
+      
+      {isPopupOpen && (
+        <Popup 
+          onClose={() => setIsPopupOpen(false)}
+        />
+      )}
+      
     </div>
     
   );
