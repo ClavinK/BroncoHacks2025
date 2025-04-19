20858fe6561eeb470c29b4ea2179c79328d224c2
JustinDrakeSantos <justin.k.santos130@gmail.com>
2025-04-18 23:51:09 -0700

    removed 2nd Navigate import

--- broncohacks2025/src/files/Landing.jsx
+++ broncohacks2025/src/files/Landing.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Landing.css'
-import { useNavigate } from 'react-router-dom';
+
 
 function LandingPage() {
     const navigate = useNavigate();
